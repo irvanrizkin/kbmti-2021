ca416156d6bd78a2b0323e821e5d5d9391f084fc
kobulwidodo <fadhilhan01@gmail.com>
2021-07-08 00:55:02 +0700

    First commit

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <img src="{{ asset('img/logo.svg') }}" alt="" width="50">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Department</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Berita</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Produk</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Open Recruitment</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
