7782ec942b8d880445a603a7473de3d923f94042
kobulwidodo <fadhilhan01@gmail.com>
2021-07-08 16:03:25 +0700

    Create footer layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,56 @@
+<section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 footer__col__cr">
+                <div class="footer__logo">
+                    <img src="{{ asset('img/logo-footer.svg') }}" alt="">
+                </div>
+                <div class="footer__alamat">
+                    Jalan Veteran Universitas Brawijaya Fakultas Ilmu Komputer Malang 65145
+                </div>
+                <div class="footer__cr d-flex">
+                    <div class="footer__cr__icon">
+                        <img src="{{ asset('img/cr-icon.svg') }}" alt="">
+                    </div>
+                    <div class="footer__cr__tahun">
+                        2021 - 2022 KBTMI
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 d-flex footer__col__mid">
+                <div class="">
+                    <div class="footer__title">Produk</div>
+                    <a href="#" class="footer__item d-block">Bank Materi</a>
+                    <a href="#" class="footer__item d-block">Bank Soal</a>
+                    <a href="#" class="footer__item d-block">Info Event TI</a>
+                    <a href="#" class="footer__item d-block">Info Beasiswa</a>
+                    <a href="#" class="footer__item d-block">Info Karir</a>
+                    <a href="#" class="footer__item d-block">TI Talks</a>
+                    <a href="#" class="footer__item d-block">Pengajuan Surat</a>
+                </div>
+            </div>
+            <div class="col-md-3 d-flex footer__col__mid">
+                <div class="">
+                    <div class="footer__title">Department</div>
+                    <a href="#" class="footer__item d-block">Ketua & Wakil</a>
+                    <a href="#" class="footer__item d-block">Internal</a>
+                    <a href="#" class="footer__item d-block">HRD</a>
+                    <a href="#" class="footer__item d-block">Advocacy</a>
+                    <a href="#" class="footer__item d-block">SE</a>
+                    <a href="#" class="footer__item d-block">RnD</a>
+                    <a href="#" class="footer__item d-block">RnC</a>
+                    <a href="#" class="footer__item d-block">Entrepreneurship</a>
+                    <a href="#" class="footer__item d-block">BPMTI</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer__title">Media Sosial</div>
+                <div class="footer__icon__medsos">
+                    <a href="#"><img src="{{ asset('img/line.svg') }}" alt=""></a>
+                    <a href="#" class="footer__icon__medsos__mid"><img src="{{ asset('img/instagram.svg') }}" alt=""></a>
+                    <a href="#"><img src="{{ asset('img/youtube.svg') }}" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
