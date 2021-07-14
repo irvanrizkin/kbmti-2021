a835cf220529b36fc66c4ef0950e64153eb2663f
kobulwidodo <fadhilhan01@gmail.com>
2021-07-14 23:05:33 +0700

    Add routes name

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('landing.home');
 
 Route::get('/department', function () {
     return view('department');
-});
+})->name('department.home');
 
 Route::get('/profile', function () {
     return view('profile');
-});
+})->name('profile.home');
