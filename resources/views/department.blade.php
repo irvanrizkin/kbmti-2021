aa333a043922336ea55ab121736182b983d67b60
kobulwidodo <fadhilhan01@gmail.com>
2021-07-19 16:45:45 +0700

    Fix responsive department top section

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -3,65 +3,75 @@
 @section('title', 'Home')
 
 @section('content')
-<section class="department__top">
-    <div class="department__top__divisi">
-        <div class="list-group department__top__list-group" id="list-tab" role="tablist">
-            <a class="list-group-item department__top__list-group__item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">EMTI</a>
-            <a class="list-group-item department__top__list-group__item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">BPMTI</a>
-        </div>
-        <div class="department__top__tab-content tab-content mb-5" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
-                <div class="department__top__tab-content__item d-flex">
-                    <img src="{{ asset('img/icon_1.svg') }}" alt="" width="48px">
-                    <a href="#">Non-Dept</a>
-                </div>
-                <div class="department__top__tab-content__item d-flex">
-                    <img src="{{ asset('img/icon_2.svg') }}" alt="" width="48px">
-                    <a href="#">Human Resource & Dev</a>
-                </div>
-                <div class="department__top__tab-content__item d-flex">
-                    <img src="{{ asset('img/icon_3.svg') }}" alt="" width="48px">
-                    <a href="#">Advocacy</a>
-                </div>
-                <div class="department__top__tab-content__item active d-flex">
-                    <img src="{{ asset('img/icon_4.svg') }}" alt="" width="48px">
-                    <a href="#">Social Environment</a>
-                </div>
-                <div class="department__top__tab-content__item d-flex">
-                    <img src="{{ asset('img/icon_5.svg') }}" alt="" width="48px">
-                    <a href="#">Research & Dev</a>
-                </div>
-                <div class="department__top__tab-content__item d-flex">
-                    <img src="{{ asset('img/icon_6.svg') }}" alt="" width="48px">
-                    <a href="#">Relation & Creative</a>
-                </div>
-                <div class="department__top__tab-content__item d-flex">
-                    <img src="{{ asset('img/icon_7.svg') }}" alt="" width="48px">
-                    <a href="#">Entrepreneurship</a>
+<div class="container-fluid">
+    <section class="department__top row">
+        <div class="col-xl-6">
+            <div class="department__top__divisi">
+                <div class="container">
+                    <div class="list-group department__top__list-group" id="list-tab" role="tablist">
+                        <a class="list-group-item department__top__list-group__item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">EMTI</a>
+                        <a class="list-group-item department__top__list-group__item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">BPMTI</a>
+                    </div>
+                    <div class="department__top__tab-content tab-content mb-5" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+                            <div class="department__top__tab-content__item d-flex">
+                                <img src="{{ asset('img/icon_1.svg') }}" alt="" width="48px">
+                                <a href="#">Non-Dept</a>
+                            </div>
+                            <div class="department__top__tab-content__item d-flex">
+                                <img src="{{ asset('img/icon_2.svg') }}" alt="" width="48px">
+                                <a href="#">Human Resource & Dev</a>
+                            </div>
+                            <div class="department__top__tab-content__item d-flex">
+                                <img src="{{ asset('img/icon_3.svg') }}" alt="" width="48px">
+                                <a href="#">Advocacy</a>
+                            </div>
+                            <div class="department__top__tab-content__item active d-flex">
+                                <img src="{{ asset('img/icon_4.svg') }}" alt="" width="48px">
+                                <a href="#">Social Environment</a>
+                            </div>
+                            <div class="department__top__tab-content__item d-flex">
+                                <img src="{{ asset('img/icon_5.svg') }}" alt="" width="48px">
+                                <a href="#">Research & Dev</a>
+                            </div>
+                            <div class="department__top__tab-content__item d-flex">
+                                <img src="{{ asset('img/icon_6.svg') }}" alt="" width="48px">
+                                <a href="#">Relation & Creative</a>
+                            </div>
+                            <div class="department__top__tab-content__item d-flex">
+                                <img src="{{ asset('img/icon_7.svg') }}" alt="" width="48px">
+                                <a href="#">Entrepreneurship</a>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-                
+        </div>
+        <div class="col-xl-6">
+            <div class="department__top__detail">
+                <div class="container text-center">
+                    <img src="{{ asset('img/blank-image.svg') }}" alt="">
+                    <div class="department__top__detail__title d-flex justify-content-center">
+                        <div class="department__top__detail__title__border"></div>
+                        <span>SE</span>
+                    </div>
+                    <div class="department__top__detail__desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Semper in placerat at in nibh. Aenean et ultrices volutpat morbi. Sit enim, sit ipsum pellentesque amet, volutpat dui lacus auctor. Fusce at ullamcorper quis mauris ut.
+                    </div>
+                    <div class="department__top__detail__scroll text-center">
+                        <span>Scroll</span>
+                        <div class="d-flex justify-content-center">
+                            <div class="department__top__detail__scroll__border"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="department__top__detail">
-        <img src="{{ asset('img/blank-image.svg') }}" alt="">
-        <div class="department__top__detail__title d-flex">
-            <div class="department__top__detail__title__border"></div>
-            <span>SE</span>
-        </div>
-        <div class="department__top__detail__desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Semper in placerat at in nibh. Aenean et ultrices volutpat morbi. Sit enim, sit ipsum pellentesque amet, volutpat dui lacus auctor. Fusce at ullamcorper quis mauris ut.
-        </div>
-        <div class="department__top__detail__scroll text-center">
-            <span>Scroll</span>
-            <div class="d-flex justify-content-center">
-                <div class="department__top__detail__scroll__border"></div>
-            </div>
-        </div>
-    </div>
 </section>
+</div>
 <section class="department__member">
     <div class="container">
         <div class="department__member__title d-flex justify-content-center">
