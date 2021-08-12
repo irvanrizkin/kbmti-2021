f4e5084e0476075858b0cc4137be11a75a2dd390
Ramdani Kurnia <adiatma85@gmail.com>
2021-08-12 09:18:54 +0700

    Add DepartmentSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             PermissionRoleTableSeeder::class,
             UsersTableSeeder::class,
             RoleUserTableSeeder::class,
+            DepartmentSeeder::class,
         ]);
     }
 }
