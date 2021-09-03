22a8a9752d76cf6c43e3fd7289053c12fd8bf4fc
kobulwidodo <fadhilhan01@gmail.com>
2021-09-03 19:31:35 +0700

    Add subfolder component

---
+++ resources/views/layouts/subfolder.blade.php
@@ -0,0 +1,6 @@
+<div class="subfolder">
+    <div class="d-flex">
+        <img class="subfolder__img" src="{{ asset('img/subfolder.svg') }}" width="24" height="24" alt="" srcset="">
+        <div class="subfolder__type">{{ $text }}</div>
+    </div>
+</div>
