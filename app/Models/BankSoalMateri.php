3b31992c1ddcf1db288ad6f150821d278e701d37
Ramdani Kurnia <adiatma85@gmail.com>
2021-09-11 21:26:04 +0700

    add database migrations and models for bank-soal-materi

---
+++ app/Models/BankSoalMateri.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use \DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BankSoalMateri extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    public const SUB_TYPE_SELECT = [
+        'DOCS' => 'docs',
+        'PPT'  => 'ppt',
+        'PDF'  => 'pdf',
+        'XLSX' => 'xlsx',
+    ];
+
+    public $table = 'bank_soal_materis';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'link',
+        'matkuliah_id',
+        'sub_type',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function matkuliah()
+    {
+        return $this->belongsTo(Matkuliah::class, 'matkuliah_id');
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
---
+++ app/Models/Matkuliah.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use \DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Matkuliah extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    public $table = 'matkuliahs';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'description',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function matkuliahBankSoalMateris()
+    {
+        return $this->hasMany(BankSoalMateri::class, 'matkuliah_id', 'id');
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
