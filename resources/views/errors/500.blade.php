76680f77257538f6c47ff7b02884c4a43e432ba8
Ramdani Kurnia <adiatma85@gmail.com>
2021-09-16 19:47:10 +0700

    add: 404 page and 500 page error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Error 500')
+
+@section('content')
+    <div class="container">
+        <section class="under_const__mid" data-aos="zoom-out" data-aos-duration="1500">
+            <div class="under_const__mid__item under_const__star_ornament animate__animated animate__bounce animate__slow animate__infinite">
+                <img src="{{ asset('img/star_ornament.svg') }}" alt="">
+            </div>
+            <div class="under_const__mid__item">
+                @include('layouts.heading', ['text' => 'Whoops...500'])
+            </div>
+            <div class="under_const__mid__item">
+                <h1 class="under_const__content text-center">
+                    Waduh, server lagi masalah nih
+                </h1>
+            </div>
+            <div class="under_const__mid__item">
+                <a class="rounded-pill under_const__button" href="{{ route("guest.landing.home") }}">
+                    Kembali
+                </a>
+            </div>
+        </section>
+    </div>
+@endsection
