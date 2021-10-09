936ba02dfd4449a38fceb60dca2d039369a192b2
Ramdani Kurnia <adiatma85@gmail.com>
2021-10-09 18:55:49 +0700

    deactivate staff muda open recruitment

--- routes/web.php
+++ routes/web.php
@@ -95,9 +95,9 @@ Route::group($routesAttributes, function () {
             Route::view('/bank-materi', 'bank_materi.index');
 
             // Open Recruitment
-            // Route::redirect('/open-recruitmen', '/under-construction')->name('open-recruitmen');
-            Route::resource('/open-recruitment', GuestOprecController::class);
-            Route::view('/open-recruitment.success', 'open-recruitment-success');
+            Route::redirect('/open-recruitmen', '/under-construction')->name('open-recruitmen');
+            // Route::resource('/open-recruitment', GuestOprecController::class);
+            // Route::view('/open-recruitment.success', 'open-recruitment-success');
 
             // Under Construction
             Route::view('/under-construction', 'under_const');
