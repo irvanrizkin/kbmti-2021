c76d79aca9198b21f1a228a65ad4d7ca80853616
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2022-02-12 16:18:49 +0700

    oprec link fix

--- resources/views/oprec/open-recruitment.blade.php
+++ resources/views/oprec/open-recruitment.blade.php
@@ -26,7 +26,7 @@
                     <div class="form-group">
                         <label class="required recruitment__label" for="name">Nama Lengkap*</label>
                         <div class="recruitment__break-small"></div>
-                        <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }} 
+                        <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}
                             search__container" style="  border-radius: 5px;" type="text" name="name" id="name"
                             value="{{ old('name', '') }}" placeholder="Jawaban Anda" required>
                         @if($errors->has('name'))
@@ -36,7 +36,7 @@
                     <div class="form-group">
                         <label class="required recruitment__label" for="nim">NIM*</label>
                         <div class="recruitment__break-small"></div>
-                        <input class="form-control {{ $errors->has('nim') ? 'is-invalid' : '' }} 
+                        <input class="form-control {{ $errors->has('nim') ? 'is-invalid' : '' }}
                             search__container" style="  border-radius: 5px;" type="text" name="nim" id="nim"
                             value="{{ old('nim', '') }}" placeholder="Jawaban Anda" required>
                         @if($errors->has('nim'))
@@ -46,7 +46,7 @@
                     <div class="form-group">
                         <label class="required recruitment__label" for="angkatan">Angkatan*</label>
                         <div class="recruitment__break-small"></div>
-                        <input class="form-control {{ $errors->has('angkatan') ? 'is-invalid' : '' }} 
+                        <input class="form-control {{ $errors->has('angkatan') ? 'is-invalid' : '' }}
                             search__container" style="  border-radius: 5px;" type="text" name="angkatan" id="prodi"
                             value="{{ old('angkatan', '') }}" placeholder="Jawaban Anda" required>
                         @if($errors->has('angkatan'))
@@ -58,7 +58,7 @@
                             <div class="form-group">
                                 <label class="required recruitment__label" for="tempat_lahir">Tempat Lahir*</label>
                                 <div class="recruitment__break-small"></div>
-                                <input class="form-control {{ $errors->has('tempat_lahir') ? 'is-invalid' : '' }} 
+                                <input class="form-control {{ $errors->has('tempat_lahir') ? 'is-invalid' : '' }}
                                     search__container" style="  border-radius: 5px;" type="text" name="tempat_lahir"
                                     id="tempat_lahir" value="{{ old('tempat_lahir', '') }}" placeholder="Jawaban Anda"
                                     required>
@@ -70,7 +70,7 @@
                             <div class="form-group">
                                 <label class="required recruitment__label" for="tanggal_lahir">Tanggal Lahir*</label>
                                 <div class="recruitment__break-small"></div>
-                                <input class="form-control {{ $errors->has('tanggal_lahir') ? 'is-invalid' : '' }} 
+                                <input class="form-control {{ $errors->has('tanggal_lahir') ? 'is-invalid' : '' }}
                                     search__container" style="  border-radius: 5px;" type="text" name="tanggal_lahir"
                                     id="tanggal_lahir" value="{{ old('tanggal_lahir', '') }}" placeholder="Jawaban Anda"
                                     required>
@@ -93,7 +93,7 @@
                     <div class="form-group">
                         <label class="required recruitment__label" for="email">Email*</label>
                         <div class="recruitment__break-small"></div>
-                        <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }} 
+                        <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}
                             search__container" style="  border-radius: 5px;" type="email" name="email" id="email"
                             value="{{ old('email', '') }}" placeholder="Jawaban Anda" required>
                         @if($errors->has('email'))
@@ -103,7 +103,7 @@
                     <div class="form-group">
                         <label class="required recruitment__label" for="id_line">ID Line*</label>
                         <div class="recruitment__break-small"></div>
-                        <input class="form-control {{ $errors->has('id_line') ? 'is-invalid' : '' }} 
+                        <input class="form-control {{ $errors->has('id_line') ? 'is-invalid' : '' }}
                             search__container" style="  border-radius: 5px;" type="id_line" name="id_line" id="id_line"
                             value="{{ old('id_line', '') }}" placeholder="Jawaban Anda" required>
                         @if($errors->has('id_line'))
@@ -113,7 +113,7 @@
                     <div class="form-group">
                         <label class="required recruitment__label" for="no_hp">No. Handphone*</label>
                         <div class="recruitment__break-small"></div>
-                        <input class="form-control {{ $errors->has('no_hp') ? 'is-invalid' : '' }} 
+                        <input class="form-control {{ $errors->has('no_hp') ? 'is-invalid' : '' }}
                             search__container" style="  border-radius: 5px;" type="text" name="no_hp" id="no_hp"
                             value="{{ old('no_hp', '') }}" placeholder="Jawaban Anda" required>
                         @if($errors->has('no_hp'))
@@ -131,7 +131,7 @@
                             <option value="Advocacy">Advocacy</option>
                             <option value="Social Environment">Social Environment</option>
                             <option value="Research and Development">Research and Development</option>
-                           
+
                             <option value="Relation and Creative">Relation and Creative</option>
                         </select>
                         @if($errors->has('pilihan1'))
@@ -152,16 +152,16 @@
                             <option value="Relation and Creative">Relation and Creative</option>
                         </select>
                         @if($errors->has('pilihan2'))
-                        @endif 
+                        @endif
                     </div>
                     <div class="recruitment__break"></div>
                     <div class="form-group">
                         <label class="required recruitment__label">Download Berkas Pendaftaran disini!</label>
                         <div class="recruitment__break-small"></div>
                         <div >
-                            <a href="https://drive.google.com/drive/folders/192rZ26fTgLwRzNQFdqa6MCQ4wOI02OwX?usp=sharing" target="blank" class="btn recruitment__button-input btn-lg ms-auto me-auto"><img src="{{ asset('img/directbox-send.svg') }}"> Download Pemberkasan (Email UB)</a>
+                            <a href="https://drive.google.com/file/d/1uH15TIjnFCYqjZva-I2ULBuqyvzt8Spv/view?usp=sharing" target="blank" class="btn recruitment__button-input btn-lg ms-auto me-auto"><img src="{{ asset('img/directbox-send.svg') }}"> Download Pemberkasan (Email UB)</a>
                         </div>
-                    </div> 
+                    </div>
                     <div class="recruitment__break"></div>
                     <div class="form-group">
                         <label class="required recruitment__label">Silahkan submit berkas kalian pada link dibawah!</label>
@@ -169,7 +169,7 @@
                         <div >
                             <a href="https://forms.gle/Z88wNbgHP8JqqGo98" target="blank" class="btn recruitment__button-input btn-lg ms-auto me-auto"><img src="{{ asset('img/directbox-send.svg') }}"> Upload Pemberkasan (Email UB)</a>
                         </div>
-                    </div> 
+                    </div>
                     <div class="recruitment__break-small"></div>
                     <div class="recruitment__break"></div>
                     <button type="submit" style="font-size: 18px;" class="btn recruitment__button-red btn-lg">
@@ -181,5 +181,5 @@
             </section>
         </div>
     </div>
-</div> 
+</div>
 @endsection
